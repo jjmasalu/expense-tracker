91891a02a567e065eb8e6f297151e125da5501c0
Don-Par <dannynduka66@gmail.com>
2025-04-30 13:54:16 -0230

    Header updated and styled a little with App.css

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Header } from './Header';
 import '../App.css';
 
 function App() {
   return (
     <div>
-      <h1>Hello world</h1>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <div>
+        <h2 className='header'>
+            Expense Tracker
+        </h2>
+    </div>
+  )
+}
